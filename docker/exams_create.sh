48ea25cc9ff21d63b40f383c2f0be6c8da3b5172
Roberto Natella <roberto.natella@unina.it>
2024-11-11 16:33:35 +0100

    deps updated, docker script parameters

--- docker/exams_create.sh
+++ docker/exams_create.sh
@@ -4,4 +4,4 @@ GROUP=$1
 NUM_USERS=$2
 REFERENCE_REPO=$3
 
-docker run --rm -it -v ./db:/app/db -v ${REFERENCE_REPO}:/reference-repo -v ./repos:/app/repos --network host  git_esami  exams_create.sh  $GROUP  ${NUM_USERS}  /reference-repo
+docker run --rm -it -v ./db:/app/db -v $(realpath ${REFERENCE_REPO}):/reference-repo -v ./repos:/app/repos --network host  git_esami  exams_create.sh  $GROUP  ${NUM_USERS}  /reference-repo
--- docker/exams_enable.sh
+++ docker/exams_enable.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -it -v ./db:/app/db --network host  git_esami  exams_enable.sh
+docker run --rm -it -v ./db:/app/db --network host  git_esami  exams_enable.sh "$@"
